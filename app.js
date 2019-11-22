94c577addb219d72cbf546b24c79741765193310
sy4may0 <sy4may0@gmail.com>
2019-11-22 11:02:16 +0900

    build config

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(logger('dev'));
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.use(cors(corsOptions));
 
 app.use('/infra', infraRouter);
